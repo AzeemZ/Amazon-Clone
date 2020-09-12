8e1730c49a98c1b887feee45ca406d62b3c81d9c
AzeemZ <azeem3z123@gmail.com>
2020-09-12 23:52:18 +0500

    Complete UI part one

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBTQ4TEAG6jfbDOGVYcm91D_1F_9pKb56Y",
+  authDomain: "clone-ce923.firebaseapp.com",
+  databaseURL: "https://clone-ce923.firebaseio.com",
+  projectId: "clone-ce923",
+  storageBucket: "clone-ce923.appspot.com",
+  messagingSenderId: "600179549685",
+  appId: "1:600179549685:web:5cc7cfc101596e9ea52cbc",
+  measurementId: "G-3MY0C4D1YN",
+};
